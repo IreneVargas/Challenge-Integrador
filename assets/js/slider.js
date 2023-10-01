f6ea7fc2000c87c9ac267b3fb3b13bdb45d86af8
Lucas Macias <lucasjmacias@gmail.com>
2023-10-01 17:41:13 -0300

    Ajuste a las imagenes slider en pantallas - 768px

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,14 +2,16 @@ new Glide(".glide", {
   type: "carousel",
   startAt: 0,
   perView: 3,
-  gap: 24,
+  gap: 30,
   keyboard: "checkbox.checked",
-  breakpoint: {
+  breakpoints: {
     768: {
       gap: 12,
+      perView: 2,
     },
     425: {
       gap: 8,
+      perView: 2,
     },
   },
 }).mount();
